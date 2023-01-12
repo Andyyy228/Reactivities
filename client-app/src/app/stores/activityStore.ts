cb2a65998278ef15db763a280fcee0c8cb7e86de
Andyyy228 <anton.bot1212@gmail.com>
2023-01-12 09:48:09 -0500

    End of section 11

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -2,6 +2,7 @@ import { makeAutoObservable, runInAction } from "mobx";
 import { Activity } from "../models/activity";
 import agent from "../api/agent";
 import { v4 as uuid } from 'uuid';
+import { format } from "date-fns";
 export default class ActivityStore {
     activityRegistry = new Map<string, Activity>();
     selectedActivity?: Activity = undefined;
@@ -12,13 +13,10 @@ export default class ActivityStore {
         makeAutoObservable(this)
     }
 
-    get groupedActivities(): [string, Activity[]][] {
-        const sortedActivities = Array.from(this.activityRegistry.values()).sort((a, b) =>
-            Date.parse(a.date) - Date.parse(b.date));
-
+    get groupedActivities() {
         return Object.entries(
-            sortedActivities.reduce((activities, activity) => {
-                const date = activity.date.split('T')[0];
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = format(activity.date!, 'dd MMM yyyy')
                 activities[date] = activities[date] ? [...activities[date], activity] : [activity];
                 return activities;
             }, {} as {[key: string]: Activity[]})
@@ -27,8 +25,9 @@ export default class ActivityStore {
 
     get activitiesByDate() {
         return Array.from(this.activityRegistry.values()).sort((a, b) =>
-            Date.parse(a.date) - Date.parse(b.date))
+            a.date!.getTime() - b.date!.getTime());
     }
+
     loadActivities = async () => {
         this.setLoadingInitial(true);
         try {
@@ -62,10 +61,12 @@ export default class ActivityStore {
             }
         }
     }
+
     private setActivity = (activity: Activity) => {
-        activity.date = activity.date.split('T')[0];
+        activity.date = new Date(activity.date!);
         this.activityRegistry.set(activity.id, activity);
     }
+
     private getActivity = (id: string) => {
         return this.activityRegistry.get(id);
     }
